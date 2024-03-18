ac1b7a48d93a9a928d527830cb8b08cc9c535513
Ohsey <admin@proton.me>
2024-03-18 19:01:05 +0000

    Begin constructing markdown data

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 
 #[cfg(feature = "html")]
 pub mod html;
+#[cfg(feature = "md")]
 pub mod md;
 #[cfg(feature = "svg")]
 pub mod svg;
--- src/md/mod.rs
+++ src/md/mod.rs
@@ -1,7 +1,45 @@
 
-pub mod headings;
-
+#[derive(Debug, PartialEq, Eq)]
 pub struct Document {
-    
+    path: Box<std::path::Path>,
+    content: Box<[DocItem]>,
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum DocItem {
+    Heading(Heading),
+    Paragraph(Paragraph),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum QuoteItem {
+    Heading(Heading),
+    Paragraph(Paragraph),
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum HeadingLevel { Level1,Level2,Level3,Level4,Level5,Level6 }
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum TextToken {
+    Def(String),
+    Bold(String),
+    Italic(String),
+    BoldItalic(String),
+    NewLine(u8),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Heading {
+    level: HeadingLevel,
+    content: Box<[TextToken]>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Paragraph(Box<[TextToken]>);
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Quote(Box<[QuoteItem]>);
+
+// impl Paragraph
 
